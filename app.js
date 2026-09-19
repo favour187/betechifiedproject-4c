dc392ba996169bae05e7e0eb293928e3a6e51c2f
moroccohabib <moroccohabib96@gmail.com>
2026-09-19 08:09:56 +0100

    install express

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const express = 'express';
+const express = require('express');
 
 const app = express()
 
+const PORT = process.env.PORT || 3000
 
 
 
 
 
 
-app.listen(8080, () => {
-    console.log(`server is running on port 8080`)
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
 })
