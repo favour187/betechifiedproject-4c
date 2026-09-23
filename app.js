4874190bcde77f6838ba4c9a3ec3732d76ee4893
Tolex081 <78969074+Tolex081@users.noreply.github.com>
2026-09-23 09:52:56 +0100

    fix: limit Member 3 contribution to mock data only

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const {randomUUID} = require('crypto');
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
-const notes = require("./data/notes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -24,6 +23,17 @@ const middlewareLogger = (req, res, next) => {
 app.use(middlewareLogger);
 
 app.use(express.json());
+
+// In-memory storage
+let notes = [
+  {
+    id: 1,
+    title: "First Note",
+    content: "This is group 4c",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
 
 // GET all notes
 app.get("/notes", (req, res) => {
@@ -79,7 +89,7 @@ app.delete('/notes/:id', (req, res) => {
         return res.status(404).json({ message: "Note not found" });
     }
 
-    notes.splice(notes.indexOf(note), 1);
+    notes = notes.filter(note => note.id !== id);
 
     res.status(200).json({ message: "Note deleted successfully" });
 });
